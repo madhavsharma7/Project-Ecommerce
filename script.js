e65061853ef6573da74e7bd73f2af6414a346099
madhav <madhav032001@gmail.com>
2024-08-11 20:43:18 +0530

    all product part complete

---
+++ script.js
@@ -0,0 +1,23 @@
+let showproductdiv = document.querySelector("#all-products");
+
+let displayproducts = async () => {
+
+  let product = await fetch("https://fakestoreapi.com/products");
+  let finalproducts = await product.json();
+  showproductdiv.innerHTML = ''; // Clear the container before adding new products
+
+  finalproducts.forEach(element => {
+    showproductdiv.innerHTML+= `
+    
+                <div class="product-items">
+                    <img src="${element.image}" alt="productimage">
+                    <h2>${element.title}</h2>
+                    <p>Category: ${element.category}</p>
+                    <p>Price: Rs ${element.price}</p>
+                    <p>Rating: ${element.rating.rate}</p>
+                </div>`;
+  });
+
+}
+
+displayproducts();
