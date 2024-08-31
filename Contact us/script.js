a994b454793d2f9e4d4dfa497bf37502e702b8e7
madhav <madhav032001@gmail.com>
2024-09-01 00:05:07 +0530

    cart problems done

--- Contact us/script.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector(".container");
-const btn = document.querySelector(".form-wrapper-left button");
-
-btn.addEventListener("click", () => {
-  container.classList.toggle("switch");
-});
