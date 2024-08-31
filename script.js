76000649291aa7a22f980625864de2d227187509
madhav <madhav032001@gmail.com>
2024-08-31 22:31:26 +0530

    done

--- script.js
+++ script.js
@@ -28,13 +28,12 @@ let displayproducts = async () => {
                     </div>
 
             </div>`;
-
     });
 
     document.querySelectorAll('.addtocartbtn').forEach((button, index) => {
         button.addEventListener("click", () => {
             let selectedProduct = finalproducts[index];
-            addtocart(selectedProduct.image, selectedProduct.title, selectedProduct.price);
+            addtocart(selectedProduct.image, selectedProduct.title, selectedProduct.price, selectedProduct.id);
         });
     });
 }
@@ -67,7 +66,7 @@ let showProductDetails = async () => {
     let addToCartButton = document.querySelector('.addtocartbtn');
     if (addToCartButton) {
         addToCartButton.addEventListener('click', () => {
-            addtocart(finalProduct.image, finalProduct.title, finalProduct.price);
+            addtocart(finalProduct.image, finalProduct.title, finalProduct.price, finalProduct.id);
         });
     } else {
         console.error('Add to Cart button not found!');
@@ -78,44 +77,49 @@ let showProductDetails = async () => {
 showProductDetails();
 
 
-let removeFromCart = (cartItemId) => {
+let removeFromCart = (cartItemId, id, price) => {
+
+     id = String(id);
     // console.log(`Attempting to remove item with ID: ${cartItemId}`); // Debugging
 
     // Find the cart item by its ID and remove it
     let cartItem = document.getElementById(cartItemId);
-    // console.log(cartItem); // Check if cartItem is found
-    if (cartItem) {
-        cartItem.remove();
-        // console.log(`Removed item with ID: ${cartItemId}`); // Debugging
+    
+        // console.log(cartItem); // Check if cartItem is found
+        if (cartItem) {
+            cartItem.remove();
+            // console.log(`Removed item with ID: ${cartItemId} and ${id}`); // Debugging
 
-        // Decrement the cart count and update the display
-        cartCount--;
-        document.getElementById('cart-count').textContent = cartCount;
+            // Decrement the cart count and update the display
+            cartCount--;
+            document.getElementById('cart-count').textContent = cartCount;
 
-        // Subtract the item's price from the total price
-        totalPrice -= parseFloat(price);
-        document.getElementById('total-price').textContent = totalPrice.toFixed(2);
+            // Subtract the item's price from the total price
+            totalPrice -= parseFloat(price);
+            document.getElementById('total-price').textContent = totalPrice.toFixed(2);
 
-    } else {
-        // console.log(`Item with ID: ${cartItemId} not found`); // Debugging
-    }
-};
-
-
-// Cart  
-
-let addtocart = (image, title, price, id) => {
+        } else {
+            // console.log(`Item with ID: ${cartItemId} not found`); // Debugging
+        }
+    };
 
 
-    // Check if the product is already in the cart
-    if (addedProductIds.has(id)) {
-        alert('This product is already in your cart.');
-        return;
-    }
+    // Cart  
 
-    let cartItemsContainer = document.querySelector('.cart-items');
-    let cartItemId = `cart-item-${cartItemIdCounter}`;
-    cartItemsContainer.innerHTML += `
+    let addtocart = (image, title, price, id) => {
+    id = String(id);
+        // Check if the product is already in the cart
+        if (addedProductIds.has(id)) {
+            alert('This product is already in your cart.');
+            return;
+        }
+
+        let cartItemsContainer = document.querySelector('.cart-items');
+        // Increment the cart item ID counter for the next item
+        cartItemIdCounter++;
+
+        let cartItemId = `cart-item-${cartItemIdCounter}`;
+        cartItemsContainer.innerHTML += `
         <div id="${cartItemId}" class="cart-item" data-id="${id}" style="display: flex; align-items: center; margin-bottom: 10px;">
             <img src="${image}" alt="product image" style="width: 50px; height: 50px; margin-right: 10px;">
             <div class="cart-item-details">
@@ -128,117 +132,111 @@ let addtocart = (image, title, price, id) => {
                   <i class="fa-solid fa-plus fa-sm plus"></i>  
                </div>
              </div>
-            <button class="dltitem" style="margin-left: 10px;" onclick="removeFromCart('${cartItemId}')">Delete</button>
+            <button class="dltitem" style="margin-left: 10px;" onclick="removeFromCart('${cartItemId}','${id}','${price}')">Delete</button>
         </div>
     `;
+        // Increment the cart count
+        cartCount++;
+        // Update the cart count display
+        document.getElementById('cart-count').textContent = cartCount;
+        
 
-    // Increment the cart count
-    cartCount++;
+        // Add the price of the newly added item to the total price
+        totalPrice += parseFloat(price);
+        document.getElementById('total-price').textContent = totalPrice.toFixed(2);
 
-    // Update the cart count display
-    document.getElementById('cart-count').textContent = cartCount;
+        // Add the product ID to the Set
+        addedProductIds.add(id);
 
-    // Increment the cart item ID counter for the next item
-    cartItemIdCounter++;
+        setupQuantityControls();
 
-    // Add the price of the newly added item to the total price
-    totalPrice += parseFloat(price);
-    document.getElementById('total-price').textContent = totalPrice.toFixed(2);
+    };
 
-    // Add the product ID to the Set
-    addedProductIds.add(id);
-
-    setupQuantityControls();
-
-};
-
-// Open cart sidebar
-document.querySelector('.fa-cart-shopping').addEventListener('click', () => {
-    document.getElementById('cart-sidebar').classList.add('active');
-});
-
-// Close cart sidebar
-document.getElementById('close-cart').addEventListener('click', () => {
-    document.getElementById('cart-sidebar').classList.remove('active');
-
-
-});
-
-
-// Cart quntity
-
-// Function to set up quantity control event listeners
-function setupQuantityControls() {
-    document.querySelectorAll('.quantity_increase').forEach(container => {
-        const quantityElement = container.querySelector('.quantity');
-        const minusButton = container.querySelector('.minus');
-        const plusButton = container.querySelector('.plus');
-        let quantity = parseInt(quantityElement.textContent);
-
-        minusButton.addEventListener('click', () => {
-            if (quantity > 1) {
-                quantity--;
-                quantityElement.textContent = quantity;
-            }
-        });
-
-        plusButton.addEventListener('click', () => {
-            quantity++;
-            quantityElement.textContent = quantity;
-        });
+    // Open cart sidebar
+    document.querySelector('.fa-cart-shopping').addEventListener('click', () => {
+        document.getElementById('cart-sidebar').classList.add('active');
     });
-}
 
+    // Close cart sidebar
+    document.getElementById('close-cart').addEventListener('click', () => {
+        document.getElementById('cart-sidebar').classList.remove('active');
 
-// Carousel
+    });
 
-let slideIndex = 1;
-showSlides(slideIndex);
+    // Cart quntity
 
-// Auto play slideshow
-let slideInterval;
+    // Function to set up quantity control event listeners
+    function setupQuantityControls() {
+        document.querySelectorAll('.quantity_increase').forEach(container => {
+            const quantityElement = container.querySelector('.quantity');
+            const minusButton = container.querySelector('.minus');
+            const plusButton = container.querySelector('.plus');
+            let quantity = parseInt(quantityElement.textContent);
 
-function startSlideInterval() {
-    slideInterval = setInterval(function () {
-        plusSlides(1);
-    }, 2000); // Change 2000 to the desired interval in milliseconds (e.g., 3000 for 3 seconds)
-}
+            minusButton.addEventListener('click', () => {
+                if (quantity > 1) {
+                    quantity--;
+                    quantityElement.textContent = quantity;
+                }
+            });
 
-function stopSlideInterval() {
-    clearInterval(slideInterval);
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+            plusButton.addEventListener('click', () => {
+                quantity++;
+                quantityElement.textContent = quantity;
+            });
+        });
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+
+
+    // Carousel
+
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Auto play slideshow
+    let slideInterval;
+
+    function startSlideInterval() {
+        slideInterval = setInterval(function () {
+            plusSlides(1);
+        }, 2000); // Change 2000 to the desired interval in milliseconds (e.g., 3000 for 3 seconds)
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
 
-// Start autoplay when the page loads
-window.onload = startSlideInterval;
+    function stopSlideInterval() {
+        clearInterval(slideInterval);
+    }
 
-// Pause autoplay when the user interacts with the slideshow
-document.querySelectorAll('.prev, .next, .dot').forEach(function (element) {
-    element.addEventListener('click', stopSlideInterval);
-});
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+
+    // Start autoplay when the page loads
+    window.onload = startSlideInterval;
+
+    // Pause autoplay when the user interacts with the slideshow
+    document.querySelectorAll('.prev, .next, .dot').forEach(function (element) {
+        element.addEventListener('click', stopSlideInterval);
+    });
 
 
 
