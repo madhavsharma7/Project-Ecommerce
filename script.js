ea345fba3022feeaaf1ab471bd0c2902326c6493
madhav <madhav032001@gmail.com>
2024-09-12 02:09:28 +0530

    done

--- script.js
+++ script.js
@@ -301,3 +301,7 @@ if (subscriptionForm) {
 }
 
 
+
+document.querySelector('.dropdown-btn').addEventListener('click', function() {
+  document.querySelector('.dropdown-content').classList.toggle('show');
+});
